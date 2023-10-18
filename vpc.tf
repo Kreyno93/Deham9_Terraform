d72e8905d79507083654c9a35fa3359c0d373646
Kreyno93 <michaelw.apple@icloud.com>
2023-10-18 16:23:53 +0200

    adding private route table

--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,34 @@
 # VPC Creation using CIDR block available in vars.tf
 
-resource "aws_vpc" "Deham9-VPC"{
-    cidr_block = var.vpc_cidr
-    enable_dns_hostnames=true
-    enable_dns_support = true
+resource "aws_vpc" "Deham9-VPC" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "Deham9-VPC"
-    }
+  tags = {
+    Name = "Deham9-VPC"
+  }
 }
 
 # Define the public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.Deham9-VPC.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "eu-north-1a"  # Change to your desired availability zone
-  map_public_ip_on_launch  = true
+  availability_zone       = "eu-north-1a" # Change to your desired availability zone
+  map_public_ip_on_launch = true
+  tags = {
+    name = "Public Subnet"
+  }
 }
 
 # Define the private subnet
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.Deham9-VPC.id
-  cidr_block              = "10.0.2.0/23"
-  availability_zone       = "eu-north-1a"  # Change to your desired availability zone
+  vpc_id            = aws_vpc.Deham9-VPC.id
+  cidr_block        = "10.0.2.0/23"
+  availability_zone = "eu-north-1a" # Change to your desired availability zone
+  tags = {
+    name = "Private Subnet"
+  }
 }
 
 # Optionally, you can create an internet gateway for the VPC and attach it to the public subnet for internet access.
@@ -36,6 +42,20 @@ resource "aws_route_table" "public_route" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags = {
+    name = "public route table"
+  }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.Deham9-VPC.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+      }
+  tags = {
+    name = "private route table"
   }
 }
 
